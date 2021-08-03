17e52c51a548e2b8db8ddcf1492ed0c87f7a0a2e
Gayane Hovhannisyan <gayaneog13@mail.ru>
2021-08-03 22:36:21 +0400

    bug fixed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,18 +2,21 @@ package com.company;
 
 
 import java.util.Scanner;
-
 import static com.company.FibonacciCalculator.fibonacciCalculator;
 
 public class Main {
-
     public static void main(String[] args) {
         // write your code here
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Input index : ");
+        System.out.println("Enter index : ");
         int number = scanner.nextInt();
-        System.out.println(number + "-th number of fibonacci is " +
-                fibonacciCalculator(number));
+        while(number<1){
+            System.out.println("index must be great then 0");
+            System.out.println("Reenter index : ");
+            number=scanner.nextInt();
+        }
+            System.out.println(number + "-th number of fibonacci is " +
+                    fibonacciCalculator(number));
 
     }
 }
